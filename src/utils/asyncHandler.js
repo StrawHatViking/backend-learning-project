0bd4dd2d8837a6cbad438f3ed892c92338dcc7a3
Uday Pratap Chauhan <udaypratapc93@gmail.com>
2025-09-27 01:23:03 +0530

    modified some files and wrote code for multer and cloudinary

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 const asyncHandler = (fn) => {
     async (req, res, next) => {
     try {
-        await fn(req, res, next)
+        return await fn(req, res, next)
     } catch (error) {
         res.status(error.code || 500).json({
             success: false,
@@ -10,3 +10,5 @@ const asyncHandler = (fn) => {
     }
 }
 }
+
+export default asyncHandler
